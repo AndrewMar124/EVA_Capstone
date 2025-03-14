9b6397a1373fe2ed270ef23bf0d63d3d923c6c6b
AndrewMar124 <martindrew01@gmail.com>
2025-03-14 13:37:55 -0400

    switching to GOlang

--- Scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ollama run EVA_qwen "user Please analyze the vulnerability report in the file at /home/EVA_Capstone/VCG_Test_Resultsphp_test_min.txt with the codebase located at /home//EVA_Capstone/VulnerableCode/VPHPA"
-    
