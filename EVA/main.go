39011ffd0415855fba77763c3847f7208ba8de5a
AndrewMar124 <martindrew01@gmail.com>
2025-03-15 16:39:20 -0400

    buggy asl but it works

--- EVA/main.go
+++ EVA/main.go
@@ -20,11 +20,77 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"encoding/csv"
 	"fmt"
 	"io"
+	"os"
 	"log"
 	"net/http"
+
 )
+
+type Vulnerability struct {
+	SeverityNumber   int    `json:"severity_number"`
+	SeverityDescription string `json:"severity_description"`
+	Vulnerability    string `json:"vulnerability"`
+	VulnDescription  string `json:"vuln_description"`
+	FileName         string `json:"file_name"`
+	LineNumber       int    `json:"line_number"`
+	LOC              string `json:"line_of_code"`
+	Confirmed        bool   `json:"confirmed"`
+	Color            string `json:"color"`
+}
+
+func csv_reader() {
+	// Open CSV file
+	file, err := os.Open("/home/stud/EVA_Capstone/VCG_Test_Results/php_results.csv")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Read CSV file
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1 // Allow variable number of fields per row
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading CSV:", err)
+		return
+	}
+
+	if len(records) < 2 {
+		fmt.Println("CSV does not contain enough data")
+		return
+	}
+
+	// Extract headers
+	headers := records[0]
+	var jsonData []map[string]string
+
+	// Convert CSV to JSON
+	for _, row := range records[1:] {
+		entry := make(map[string]string)
+		for i, value := range row {
+			if i < len(headers) {
+				entry[headers[i]] = value
+			}
+		}
+		jsonData = append(jsonData, entry)
+	}
+
+	// Convert to JSON format
+	jsonOutput, err := json.MarshalIndent(jsonData, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
+	}
+
+	// Print JSON output
+	fmt.Println(string(jsonOutput))
+}
+
+
 
 func main() {
 	// Prepare the JSON body
@@ -112,4 +178,6 @@ func main() {
 	} else {
 		log.Println("No 'response' field found in the JSON response.")
 	}
+
+	csv_reader()
 }
