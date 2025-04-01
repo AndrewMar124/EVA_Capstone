d99d9eeb1957c7e52979b8b38f5a2b5406bfb775
AndrewMar124 <martindrew01@gmail.com>
2025-04-01 19:17:30 -0400

    pj file uploads

--- EVA/serve.go
+++ EVA/serve.go
@@ -1,28 +1,91 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-    http.HandleFunc("/", serveDash)
+	http.HandleFunc("/", serveDash)
 	http.HandleFunc("/vuln", serveVulnerabilities)
+	http.HandleFunc("/proj", serveProj)
+	
+	http.HandleFunc("/createProject", createProject)
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    fmt.Println("Server started on http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func serveDash(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "dash.html")
 }
+
+func serveProj(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "proj.html")
+}
+
+func createProject(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+        return
+    }
+
+    err := r.ParseMultipartForm(10 << 20)
+    if err != nil {
+        http.Error(w, "Unable to parse form", http.StatusBadRequest)
+        return
+    }
+
+    projectName := r.FormValue("name")
+    if projectName == "" {
+        http.Error(w, "Project name is required", http.StatusBadRequest)
+        return
+    }
+
+    file, handler, err := r.FormFile("file")
+    if err != nil {
+        http.Error(w, "Unable to get the file from the form", http.StatusBadRequest)
+        return
+    }
+    defer file.Close()
+
+    filePath := filepath.Join("uploads", handler.Filename)
+
+    dst, err := os.Create(filePath)
+    if err != nil {
+        http.Error(w, "Unable to create the file", http.StatusInternalServerError)
+        return
+    }
+    defer dst.Close()
+
+    _, err = dst.ReadFrom(file)
+    if err != nil {
+        http.Error(w, "Unable to save the file", http.StatusInternalServerError)
+        return
+    }
+
+    db := conn_psql()
+    defer db.Close()
+
+    query := "INSERT INTO project (name, sca) VALUES ($1, $2)"
+    _, err = db.Exec(query, projectName, filePath)
+    if err != nil {
+        http.Error(w, "Failed to insert project into database", http.StatusInternalServerError)
+        return
+    }
+
+	http.Redirect(w, r, r.Header.Get("/proj"), http.StatusSeeOther)
+}
+
 
 func serveVulnerabilities(w http.ResponseWriter, r *http.Request) {
 	db := conn_psql()
@@ -42,20 +105,20 @@ func serveVulnerabilities(w http.ResponseWriter, r *http.Request) {
 
 		// Explicitly map database columns to struct fields
 		err := rows.Scan(
-			&v.ID,                   // Column: id
-			&v.Color,                // Column: color
-			&v.Confirmed,            // Column: confirmed
-			&v.FileContents,         // Column: file_contents
-			&v.Filename,             // Column: filename
-			&v.LOC,                  // Column: loc
-			&v.LineNumber,           // Column: line_number
-			&v.SeverityDescription,  // Column: severity_description
-			&v.SeverityNumber,       // Column: severity_number
+			&v.ID,                  // Column: id
+			&v.Color,               // Column: color
+			&v.Confirmed,           // Column: confirmed
+			&v.FileContents,        // Column: file_contents
+			&v.Filename,            // Column: filename
+			&v.LOC,                 // Column: loc
+			&v.LineNumber,          // Column: line_number
+			&v.SeverityDescription, // Column: severity_description
+			&v.SeverityNumber,      // Column: severity_number
 			&v.VulnDescription,     // Column: vuln_description
-			&v.Vulnerability,        // Column: vulnerability
-			&v.Reason,               // Column: reason
-			&v.Verification,         // Column: verification
-			&v.CreatedAt,            // Column: created_at
+			&v.Vulnerability,       // Column: vulnerability
+			&v.Reason,              // Column: reason
+			&v.Verification,        // Column: verification
+			&v.CreatedAt,           // Column: created_at
 		)
 		if err != nil {
 			http.Error(w, "Error reading data", http.StatusInternalServerError)
@@ -73,6 +136,6 @@ func serveVulnerabilities(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-    // Render template with vulnerabilities data
-    tmpl.Execute(w, vulnerabilities)
+	// Render template with vulnerabilities data
+	tmpl.Execute(w, vulnerabilities)
 }
